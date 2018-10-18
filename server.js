85dc8826ee7ae97935394c0d1c86627603cc856c
Korn <Tana_korn01@hotmail.com>
2018-10-18 17:29:11 +0700

    trtrtrtrtr

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post('/products/update', function (request, response){
                 
     })
 });
-app.get('/products/delete/:pid', function (request, response) {
+app.get('/product_delete/:pid', function (request, response) {
     var id = request.param('id');
     var sql = 'delete from products';
     if (id){
